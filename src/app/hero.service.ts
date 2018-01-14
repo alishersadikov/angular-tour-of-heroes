c1b8e68cdd9b6d92a9541a3c6cc32b48b2de66d4
Alisher Sadikov <asadikov@msts.com>
2018-01-14 17:31:39 -0600

    Implement dynamic routing for detail and other enhancements

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -7,6 +7,12 @@ import { MessageService } from './message.service';
 
 @Injectable()
 export class HeroService {
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
+
   getHeroes(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
@@ -14,5 +20,4 @@ export class HeroService {
   }
 
   constructor(private messageService: MessageService) { }
-
 }
